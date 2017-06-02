8cab41acddfee3e379fdbcadafa3f1f3ce705aee
Diogo Pereira <diogopereira@pereiradiogo.com>
2017-06-02 16:50:45 +0100

    ok

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,6 @@ router.get('/', function(req, res, next) {
   res.render('index', json);
 });
 
-
-
 request('http://localhost:3000/random.json', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred 
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
